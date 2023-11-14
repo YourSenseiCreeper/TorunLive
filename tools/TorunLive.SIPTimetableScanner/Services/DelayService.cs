02c3ee914d8c53e3ec211d75cf5a23d3cc908c57
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2023-11-14 12:05:33 +0100

    Delay configurable from appsettings

--- tools/TorunLive.SIPTimetableScanner/Services/DelayService.cs
+++ tools/TorunLive.SIPTimetableScanner/Services/DelayService.cs
@@ -1,12 +1,19 @@
-﻿using TorunLive.SIPTimetableScanner.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using TorunLive.SIPTimetableScanner.Interfaces;
 
 namespace TorunLive.SIPTimetableScanner.Services
 {
     public class DelayService : IDelayService
     {
+        private readonly int _requestDelayBaseInMiliseconds;
+        public DelayService(IConfiguration configuration)
+        {
+            _requestDelayBaseInMiliseconds = configuration.GetValue<int>("RequestDelayBaseInMiliseconds");
+        }
+
         public async Task Delay()
         {
-            var innerDelay = (int)(200 * new Random().NextDouble());
+            var innerDelay = (int)(_requestDelayBaseInMiliseconds * new Random().NextDouble());
             await Task.Delay(innerDelay);
         }
     }
