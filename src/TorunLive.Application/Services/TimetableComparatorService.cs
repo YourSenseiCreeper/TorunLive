d33de38b3559a372ad56fab5dce48dcf57b762bf
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2023-11-20 12:24:49 +0100

    Timetable comparator service tests

--- src/TorunLive.Application/Services/TimetableComparatorService.cs
+++ src/TorunLive.Application/Services/TimetableComparatorService.cs
@@ -11,6 +11,23 @@ namespace TorunLive.Application.Services
 
         public IEnumerable<CompareLine> Compare(LiveTimetable baseTimetable, LiveTimetable liveTimetable)
         {
+            //var liveLinesThatExistInBaseTimetable = liveTimetable.Lines.Where(lt => baseTimetable.Lines.Any(l => l.Number == lt.Number));
+            //var liveLinesThatExistInBaseTimetable = liveTimetable.Lines
+            //    .Join(baseTimetable.Lines,
+            //        live => live.Number,
+            //        baseLine => baseLine.Number,
+            //        (live, baseLine) => live);
+
+            //var missingBaseLines = baseTimetable.Lines.Except(liveLinesThatExistInBaseTimetable)
+            //    .Select(l => new CompareLine
+            //    {
+            //        Name = l.Name,
+            //        Number = l.Number,
+            //        Error = $"ERR: Nie znaleziono lini na liście linini z rozkładu"
+            //    });
+            //foreach (var missingLine in missingBaseLines)
+            //    yield return missingLine;
+
             foreach (var liveLine in liveTimetable.Lines)
             {
                 var comparedLine = new CompareLine
--- tests/TorunLive.Application.Tests/Services/TimetableComparatorServiceTests.cs
+++ tests/TorunLive.Application.Tests/Services/TimetableComparatorServiceTests.cs
@@ -37,6 +37,7 @@ namespace TorunLive.Application.Tests.Services
             result.Count.ShouldBe(1);
             result[0].Number.ShouldBe(lineNumber);
             result[0].Name.ShouldBe(null);
+            result[0].Error.ShouldBeNullOrEmpty();
             result[0].Arrivals.Count.ShouldBe(1);
             result[0].Arrivals[0].BaseDayMinute.ShouldBe(baseArrival);
             result[0].Arrivals[0].Delay.ShouldBe(baseArrival - liveArrival);
@@ -44,7 +45,7 @@ namespace TorunLive.Application.Tests.Services
         }
 
         [Test]
-        public void Compare_TwoBaseLinesOneLiveLineOneArrival_OneErrorMessage()
+        public void Compare_TwoBaseLinesOneLiveLineOneArrival_OneResult()
         {
             const int currentDayMinute = 600;
             const int baseArrival = currentDayMinute + 3;
@@ -54,7 +55,8 @@ namespace TorunLive.Application.Tests.Services
             {
                 Lines =
                 [
-                    new LiveLine { Number = lineNumber, ArrivalsInDayMinutes = [baseArrival] }
+                    new LiveLine { Number = lineNumber, ArrivalsInDayMinutes = [baseArrival] },
+                    new LiveLine { Number = "11", ArrivalsInDayMinutes = [baseArrival + 5] }
                 ]
             };
             var liveTimetable = new LiveTimetable
@@ -71,10 +73,39 @@ namespace TorunLive.Application.Tests.Services
             result.Count.ShouldBe(1);
             result[0].Number.ShouldBe(lineNumber);
             result[0].Name.ShouldBe(null);
+            result[0].Error.ShouldBeNullOrEmpty();
             result[0].Arrivals.Count.ShouldBe(1);
             result[0].Arrivals[0].BaseDayMinute.ShouldBe(baseArrival);
             result[0].Arrivals[0].Delay.ShouldBe(baseArrival - liveArrival);
             result[0].Arrivals[0].LiveDayMinute.ShouldBe(liveArrival);
+        }
+
+        [Test]
+        public void Compare_OneBaseLineOneDifferentLiveLineOneArrival_OneErrorMessage()
+        {
+            const int currentDayMinute = 600;
+            const int baseArrival = currentDayMinute + 3;
+            const int liveArrival = currentDayMinute + 5;
+            const string lineNumber = "10";
+            var baseTimetable = new LiveTimetable()
+            {
+                Lines =
+                [
+                    new LiveLine { Number = "11", ArrivalsInDayMinutes = [baseArrival + 5] }
+                ]
+            };
+            var liveTimetable = new LiveTimetable
+            {
+                Lines =
+                [
+                    new LiveLine { Number = lineNumber, ArrivalsInDayMinutes = [liveArrival] }
+                ]
+            };
+            var service = new TimetableComparatorService();
+
+            var result = service.Compare(baseTimetable, liveTimetable).ToList();
+
+            result.Count.ShouldBe(0);
         }
     }
 }
